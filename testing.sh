f8e06c9c87bfc4ef525854b730dfe63ac9eab99b
Theory5 <Theory5@users.noreply.github.com>
2014-05-14 14:33:30 -0400

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -5,6 +5,7 @@
 ROOT_UID=0     # Only users with $UID 0 have root privileges.
 E_NOTROOT=87   # Non-root exit error.
 UID=$(id -u)
+
 # Run as root, of course.
 if [ "$UID" -ne "$ROOT_UID" ]; then
   echo "Must be root to run this script."
