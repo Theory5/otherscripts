902408fb0795c26d0d9345b84bd4cd2465cae999
Theory5 <Theory5@users.noreply.github.com>
2014-05-14 15:22:47 -0400

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -28,7 +28,7 @@ dpkg-query -W -f='${Package}\n' > $WORKDIR/packlist1.txt
 if [ -n "$1" ];then
 STARTT=$(date +%M)
 file="$1"
-sh "$file" | readline echo 1> stdout.txt 2> stderr.txt
+/bin/bash "$file" | readline echo 1> stdout.txt 2> stderr.txt
 ENDT=$(date +%M)
 TIME1=$( $ENDT - $STARTT )
 else
