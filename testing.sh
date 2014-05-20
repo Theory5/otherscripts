32ccddd050a878fac940ac48c14c6ccc73bb5c0c
Theory5 <Theory5@users.noreply.github.com>
2014-05-20 13:54:35 -0400

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -26,10 +26,10 @@ dpkg-query -W -f='${Package}\n' > $WORKDIR/packlist1.txt
 #Get script to run via command line by using script to be tested as argument
 
 if [ -n "$1" ];then
-STARTT=$(date +%s)
+STARTT=$(date +%s%N)
 file="$1"
-/bin/bash "$file" | readline echo 1> stdout.txt 2> stderr.txt
-ENDT=$(date +%s)
+/bin/bash "$file" | read line echo 1> stdout.txt 2> stderr.txt
+ENDT=$(date +%s%N)
 TIME1=$($ENDT - $STARTT)
 else
 
@@ -39,13 +39,13 @@ return
 
 fi  
 
-TIME2=$(( 60 / $TIME1 ))
+TIME2=$(( $TIME1 / 60 ))
 
 #find all files modified in the time of $TIME1 and print to new file for logging purposes
 find -ignore_readdir_race -mmin $TIME2 -mmin -$TIME2 -path $DIRLIST1 -prune -o -print > findtime.log
 
 #get list of all packages with modified permissions
-ls -la "$(<findtime.log)"
+ls -la "$(<findtime.log)" tee echo > $WORKDIR/modfiles.txt 2> /dev/null
 #run dpkg again to get 2nd list for comparison, again strip everything but package name
 
 dpkg-query -W -f='${Package}\n' > $WORKDIR/packlist2.txt
@@ -57,33 +57,33 @@ grep -Fxvf packlist1.txt packlist2.txt > packexc.txt
 
 #clean up, remove packages, remove all added files
 
-read -p "Do you want to delete all files created during the running of $file? (Yes/No)" ANS1
-  if [ "$ANS1" =  "yes" ]; then
+read -p "Do you want to delete all files created during the running of $file? (y/n)" ANS1
+  if [ "$ANS1" =  "y" ]; then
                 echo "Deleting Files... Please Wait"
                 rm -rf "$(< findtime.txt)"
-      elif [ "$ANS1" = "no" ]; then
+      elif [ "$ANS1" = "n" ]; then
            echo "Nothing Will Be Deleted. Good Day!"     
             else
                 echo "I do not understand that value, please choose yes or no"
             fi
             
-read -p "Do you want to remove all packages that were installed during the running of $file? (Yes/No)" ANS2
-  if [ "$ANS2" = "yes" ]; then
+read -p "Do you want to remove all packages that were installed during the running of $file? (y/n)" ANS2
+  if [ "$ANS2" = "y" ]; then
                 echo "Uninstalling/Removing Packages... Please Wait"
                 apt-get -y remove "$(<packexc.txt)"
                 apt-get -y purge "$(<packexc.txt)"
-      elif [ "$ANS2" = "no" ]; then
+      elif [ "$ANS2" = "n" ]; then
            echo "Nothing Will Be Deleted. Good Day!"     
             else
                 echo "I do not understand that value, please choose yes or no"
             fi
 
-read -p "Do you want to initiate final cleanup? All log files will be moved to the backup directory? (Yes/No)" ANS3
-if [ "$ANS3" =  "yes" ]; then
+read -p "Do you want to initiate final cleanup? All log files will be moved to the backup directory? (y/n)" ANS3
+if [ "$ANS3" =  "y" ]; then
                 echo "Moving Log Files... Please Wait"
                 FOLD=$((mkdir "$WORKDIR/`date +%k%M%a`"))
                 mv 'packlist1.txt packlist2.txt packexc.txt findtime.txt stdout.txt stderr.txt' '$WORKDIR/$FOLD'
-      elif [ "$ANS3" = "no" ]; then
+      elif [ "$ANS3" = "n" ]; then
            echo "Nothing Will Be Moved, The Files Will Stay in $WORKDIR"     
             else
                 echo "I do not understand that value, please choose yes or no"
